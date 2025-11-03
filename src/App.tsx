c1ce5210478785aed23a39afd7ed20d313c48f1f
jackal7819 <jackal3166@gmail.com>
2025-11-03 17:07:03 +0200

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 export default function App() {
 	return (
 		<main>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<div className='pattern' />
+
+			<div className='wrapper'>
+				<header>
+					<img src='./hero.png' alt='Hero Banner' />
+					<h1>
+						Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the
+						Hassle
+					</h1>
+				</header>
+
+				<p>Search</p>
+			</div>
 		</main>
 	);
 }
